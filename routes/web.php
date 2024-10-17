c05dca728a69d676a9f5aa1192e5ae2abdf7c9fa
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-10-17 18:00:45 -0300

    -- Criando a página de login e a página home, usando layouts e o blade -- Também foi adicionando bootstrap e os links necessários

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    echo "Hello fortify";
+    return view('home');
 })->middleware(['auth']); //Ao adicionar esse middleware, o fortify identifica se está autenticado e se não, redireciona para auth.login
